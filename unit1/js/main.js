fd24ede6448752a24d04d0bb95ba88a68e41c680
Christian Hur <chrisvhur@gmail.com>
2020-07-31 15:31:07 -0500

    Add files via upload

---
+++ unit1/js/main.js
@@ -0,0 +1,26 @@
+
+function show() {
+    var video = document.getElementById("video");
+    var audio = document.getElementById("audio");
+    video.style.display = "block";
+    audio.style.display = "block";
+}
+
+
+function hide() {
+    var video = document.getElementById("video");
+    var audio = document.getElementById("audio");
+    video.style.display = "none";
+    audio.style.display = "none";
+}
+//
+// var rec1 = [101,"Kevin Smith","JavaScript 2","A-","Winter","2019"];
+// var rec2 = [102,"Lucy Lane","JavaScript 2","A","Winter","2019"];
+//
+// function return2DArray(rec1, rec2) {
+//
+//     return new Array(rec1,rec2);
+//
+// }
+//
+// console.log(return2DArray(rec1,rec2));
